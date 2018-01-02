7b42945f4633780452f7d6dbd9b2ee341fbb297c
Raphael Elvis Rozario <richie.rozario07@gmail.com>
2018-01-02 21:55:56 +0600

    Initial commit

---
+++ ASA/Controllers/HomePageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASA.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: test
+        public ActionResult homepage()
+        {
+            return View();
+        }
+
+        public ActionResult contact()
+        {
+            return View();
+        }
+
+      
+    }
+}
